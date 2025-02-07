48126e31ead0ffd43d0b265d47e0a87538bb7ec0
EC2 Default User <ec2-user@ip-172-31-0-87.ap-northeast-1.compute.internal>
2025-02-07 06:47:30 +0000

    pokemondate-api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
+import {useEffect} from "react";
+import {useState} from "react";
 import logo from './logo.svg';
 import './App.css';
-
+import {getAllPokemon} from "./utils/mokemon.js";
 function App() {
-  const initialURL = "https://pokeapi.co/api/v2/";
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading ,setLoding] = useState(true);
   
   useEffect(() => {
     const fetchPokemonDate = async () => {
       //全てのポケモンデータを取得
-      let les = await getAllPokemon();
-    }
+      let les = await getAllPokemon(initialURL);
+      console.log(les);
+      setLoding(false);
+    };
+    fetchPokemonDate();
   });
   return (
-    <div className="App"></div>
+    <div className="App">
+      {loading ? (
+        <h1>ローディング中…</h1>
+        ) : (
+        <>  
+          <h1>
+          ポケモンのデータを取得しました  
+          </h1>
+        </>
+          )}
+    </div>      
   );
 }
 
--- src/utils/mokemon.js
+++ src/utils/mokemon.js
@@ -1,3 +1,7 @@
-export const getAllPokemon = () => {
-    
+export const getAllPokemon = (url) => {
+    return new Promise((resolve ,reject) => {
+        fetch(url)
+        .then((res) => res.json())
+        .then((data) => resolve(data));
+    });
 }
